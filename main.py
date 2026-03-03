71c2b2ad2114ad2e13e50ec60e09283dce2d254c
Bekmurod <bekmurodgofurov0000@gmail.com>
2026-03-03 15:22:18 +0500

    it is working

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hellow World")
+import hashlib
+
+def get_file_hash(filename):
+    # Faylni bayt ko'rinishida o'qiymiz
+    with open(filename, "rb") as f:
+        bytes = f.read() 
+        # SHA-256 algoritmi orqali hash hisoblaymiz
+        readable_hash = hashlib.sha256(bytes).hexdigest()
+        return readable_hash
+
+# Ishlatib ko'ramiz:
+file_name = "test.txt" # Kompyuteringizdagi fayl nomi
+print(f"Fayl hashi: {get_file_hash(file_name)}")
